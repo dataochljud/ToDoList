0fa62189bac430214a4f423135cf925585b01a7a
Data och Ljud <johan@dataochljud.se>
2020-04-27 14:06:59 +0200

    Added GPLv3 text

--- shared/ListItem.cpp
+++ shared/ListItem.cpp
@@ -1,3 +1,27 @@
+/*
+This file is part of ToDoList.
+
+    ToDoList is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+/* File: ListItem.cpp
+   
+   written by: Johan Tibbelin (johan@dataochljud.se)
+
+   Last changed: 2020-04-27 14:06:30
+
+   Description: class for the List items the main data of the program.  
+ */
 #include <cstdio>
 #ifdef __ATARI_ST__
 #include <osbind.h>

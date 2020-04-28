bd9e99f033c886045a40a289211a5a6ca2a878c0
Data och Ljud <johan@dataochljud.se>
2020-04-28 10:42:16 +0200

    iOS App added

---
+++ iOS/ToDoList/ToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Johan Roos Tibbelin on 2020-04-28.
+//  Copyright © 2020 Data och Ljud. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
